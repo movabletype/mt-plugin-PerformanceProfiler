b6915f612ee37c7b285b927982c3c5f8784efcea
Taku Amano <taku@taaas.jp>
2021-03-25 12:01:01 +0900

    Use KYTProf by default.

---
+++ addons/PerformanceProfiler.pack/lib/MT/Plugin/PerformanceProfiler.pm
@@ -0,0 +1,148 @@
+package MT::Plugin::PerformanceProfiler;
+
+use strict;
+use warnings;
+use utf8;
+
+use File::Spec;
+use File::Basename qw(basename);
+use MT::Plugin::PerformanceProfiler::KYTProfLogger;
+
+use constant FILE_PREFIX => 'b-';
+
+sub path {
+    MT->config->PerformanceProfilerPath;
+}
+
+sub enabled {
+    return !!path();
+}
+
+sub profiler_enabled {
+    my ($profiler) = @_;
+    return grep { $_ eq $profiler }
+        split( /\s*,\s*/, MT->config('PerformanceProfilerProfilers') );
+}
+
+sub enable_profile {
+    my ($file) = @_;
+
+    if ( profiler_enabled('NYTProf') ) {
+        DB::enable_profile( sprintf( $file, 'nyt' ) );
+    }
+
+    if ( profiler_enabled('KYTProf') ) {
+
+        # XXX: force re-initialize $Devel::KYTProf::Profiler::DBI::_TRACER
+        Devel::KYTProf::Profiler::DBI->apply;
+        Devel::KYTProf->logger(
+            MT::Plugin::PerformanceProfiler::KYTProfLogger->new( sprintf( $file, 'kyt' ) ) );
+    }
+}
+
+sub finish_profile {
+
+    if ( profiler_enabled('NYTProf') ) {
+        DB::finish_profile();    # NYTProf
+    }
+
+    if ( profiler_enabled('KYTProf') ) {
+
+        # FIXME: We want to release DBIx::Tracer in an implementation-independent way
+        if ($Devel::KYTProf::Profiler::DBI::_TRACER) {
+            Devel::KYTProf->mute($_) for qw(DBI DBI::st DBI::db);
+            undef $Devel::KYTProf::Profiler::DBI::_TRACER;
+            Devel::KYTProf->_orig_code( {} );
+            Devel::KYTProf->_prof_code( {} );
+        }
+    }
+}
+
+sub cleanup {
+    my $max_files = MT->config->PerformanceProfilerMaxFiles
+        or return;    # unlimited
+
+    my %files = ();
+    for my $f ( grep { -f $_ } glob( File::Spec->catfile( path(), FILE_PREFIX . '*' ) ) ) {
+        my $bn = basename($f);
+        my ($profiler) = $bn =~ m/@{[FILE_PREFIX()]}([a-zA-Z]+)/;
+        $files{$profiler} ||= [];
+        push @{ $files{$profiler} }, $f;
+    }
+
+    for my $profiler ( keys %files ) {
+        my @files = map { $_->[1] }
+            sort { $b->[0] <=> $a->[0] }
+            map  { [ ( stat($_) )[10] => $_ ] } @{ $files{$profiler} };
+        for my $f ( @files[ $max_files - 1 .. $#files ] ) {
+            unlink $f;
+        }
+    }
+}
+
+sub init_app {
+    my $dir = path()
+        or return;
+
+    if ( profiler_enabled('KYTProf') ) {
+        require Devel::KYTProf;
+        Devel::KYTProf->apply_prof('DBI');
+        Devel::KYTProf->namespace_regex('MT::Template');
+    }
+
+    if ( profiler_enabled('NYTProf') ) {
+        my @opts = qw( sigexit=int savesrc=0 start=no );
+        $ENV{"NYTPROF"} = join ":", @opts;
+        require Devel::NYTProf;
+    }
+
+    mkdir $dir unless -d $dir;
+
+    finish_profile();
+
+    1;
+}
+
+sub build_file_filter {
+    my ( $cb, %param ) = @_;
+
+    my $dir = path()
+        or return;
+
+    finish_profile();
+
+    my $freq = MT->config->PerformanceProfilerFrequency
+        or return;    # disabled
+    return unless int( rand($freq) ) == 0;
+
+    return unless -d $dir;
+
+    my $filename = $param{File};
+    $filename =~ s{^/|/$}{}g;
+    $filename =~ s{[^0-9a-zA-Z_-]}{_}g;
+
+    cleanup();
+    enable_profile( File::Spec->catfile( $dir, FILE_PREFIX . '%s-' . $filename ) );
+
+    1;
+}
+
+sub build_page {
+    my ($cb) = @_;
+
+    enabled() or return;
+    finish_profile();
+
+    1;
+}
+
+sub take_down {
+    my ($cb) = @_;
+
+    enabled() or return;
+    finish_profile();
+
+    1;
+}
+
+1;
---
+++ addons/PerformanceProfiler.pack/lib/MT/Plugin/PerformanceProfiler/KYTProfLogger.pm
@@ -0,0 +1,42 @@
+package MT::Plugin::PerformanceProfiler::KYTProfLogger;
+
+use strict;
+use warnings;
+use utf8;
+
+use MT::Util ();
+use MT::Util::Digest::SHA;
+
+sub new {
+    my $class = shift;
+    my ($file_name) = @_;
+    open my $fh, '>', $file_name;
+    my $self = { fh => $fh };
+    bless $self, $class;
+    $self;
+}
+
+sub log {
+    my $self = shift;
+    my %args = @_;
+
+    my @binds = map {
+              $_ =~ m{\A(?:[0-9]+|undef)?\z}
+            ? $_
+            : substr( MT::Util::Digest::SHA::sha1_hex($_), 0, 8 );
+    } split( /, /, ( $args{data}{sql_binds} =~ m{\A\(bind: (.*)\)\z} )[0] );
+
+    print { $self->{fh} } MT::Util::to_json(
+        {   runtime          => $args{time},
+            operation_class  => $args{module},
+            operation_method => $args{method},
+            caller_package   => $args{package},
+            caller_file_name => $args{file},
+            caller_line      => $args{line},
+            sql              => $args{data}{sql},
+            sql_binds        => \@binds,
+        }
+    ) . "\n";
+}
+
+1;
